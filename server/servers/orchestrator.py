4700068424d78455ee826b43e2954f46e0687e3f
Seturaman Kumar <seturamankumar@gmail.com>
2026-02-26 00:07:06 +0530

    Added minecraft serevr docker containerization script

---
+++ server/servers/orchestrator.py
@@ -0,0 +1,68 @@
+import docker
+import time
+
+client = docker.from_env()
+
+def start_minecraft_server(server_name, port_number, mc_version="LATEST", allow_tlauncher=False, max_players=20, difficulty="normal"):
+    print(f"Preparing to start {server_name} on port {port_number}")
+
+    env_vars = {
+        "EULA": "TRUE",
+        "TYPE": "PAPER",
+        "MEMORY": "1G",
+        "VERSION": mc_version,
+        "MAX_PLAYERS": str(max_players),
+        "DIFFICULTY": difficulty,
+        "ENABLE_AUTOSTOP": "TRUE",
+        "AUTOSTOP_TIMEOUT_EST": "300",
+    }
+
+    if allow_tlauncher:
+        env_vars["ONLINE_MODE"] = "FALSE"
+        print("Tlauncher support enabled (Online Mode: OFF)")
+    else:
+        env_vars["ONLINE_MODE"] = "TRUE"
+        print("Official Mojang authentication enable (Online Mode: On)")
+
+    try:
+        container = client.containers.run(
+            "itzg/minecraft-server",
+            name=server_name,
+            detach=True,
+            environment=env_vars,
+            ports={'25565/tcp': port_number},
+            remove=True,
+        )
+
+        print(f"Success! Container {container.short_id} is booting woth max {max_players} on {difficulty} difficulty.")
+        return container
+    
+    except docker.errors.APIError as e:
+        print(f"Failed to start container: {e}")
+        return None
+    
+def stop_minecraft_server(container):
+    print(f"Sending stop command to {container.name}...")
+    container.stop(timeout=30)
+    print("Server gracefully stopped and completely removed")
+
+if __name__ == "__main__":
+    print("--- SquadHost Interactive Engine Test ---")
+
+    test_container =  start_minecraft_server(
+        server_name="squadhost_custom_01",
+        port_number=25570,
+        mc_version="1.20.4",
+        allow_tlauncher=True,
+        max_players=5,
+        difficulty="hard",
+    )
+
+    if test_container:
+        print("Letting the custom server generate for 15 seconds")
+        time.sleep(15)
+
+        input("\n>> SERVER IS BOOTING! Open Minecraft (v1.20.4). \n>> Connect to IP: localhost:25570 \n>> Press ENTER in this terminal ONLY when you are ready to shut it down and delete the server...\n")
+
+        stop_minecraft_server(test_container)
+        print("Custom server test complete")
