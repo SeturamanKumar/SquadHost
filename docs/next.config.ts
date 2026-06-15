bb86b0718bd9f62ffd7231e0d425234e5074bbef
Seturaman Kumar <seturamankumar@gmail.com>
2026-06-15 13:12:07 +0530

    Change next.config.ts for documentation back to original

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -3,11 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  images: { unoptimized: true },
-  allowedDevOrigins: ['::'],
+  // images: { unoptimized: true },
+  // allowedDevOrigins: ['::'],
 };
-
-module.exports = {
-}
 
 export default nextConfig;
